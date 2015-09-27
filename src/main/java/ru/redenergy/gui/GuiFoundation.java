d3a636ee2a210823ba47e1e09b306a918a677482
RedEnergy <forwot163@gmail.com>
2015-09-27 17:06:55 +0400

    Aded java 8 compatibility, removed third-party library calls

--- src/main/java/ru/redenergy/gui/GuiFoundation.java
+++ src/main/java/ru/redenergy/gui/GuiFoundation.java
@@ -1,26 +1,15 @@
 package ru.redenergy.gui;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.logging.log4j.core.Logger;
-import org.lwjgl.input.Keyboard;
-
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import net.minecraft.client.Minecraft;
-import ru.redenergy.gui.example.ExampleGuiPane;
-import skymine.redenergy.core.Core;
 
-@Mod(modid = "gfoundation", name = "GuiFoundation Library by RedEnergy")
+@Mod(modid = "gfoundation", name = "Daozi Gui Library by RedEnergy")
 public class GuiFoundation {
 
 	@Mod.EventHandler
 	public void postLoad(FMLPostInitializationEvent event){
 		FMLLog.info("GuiFoundation has been successfuly initialized");
-		Core.getKeyboardHandler().registerKeyBinding(Keyboard.KEY_L, "s", (ev) -> {
-			Minecraft.getMinecraft().displayGuiScreen(new ExampleGuiPane());
-		});
 	}
 	
 }
