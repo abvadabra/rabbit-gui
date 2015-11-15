cafb8c7b1db24e3f93ca928ea81affb3934cfbd9
RedEnergy <forwot163@gmail.com>
2015-11-15 15:53:56 +0400

    TextRenderer: integer colors changed to java.awt.Color

--- src/main/java/com/rabbit/gui/render/TextRenderer.java
+++ src/main/java/com/rabbit/gui/render/TextRenderer.java
@@ -1,5 +1,7 @@
 package com.rabbit.gui.render;
 
+import java.awt.Color;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 
@@ -16,7 +18,7 @@ public class TextRenderer {
      */
     @Deprecated
     public static int renderCenteredString(int xPos, int yPos, String text) {
-        return renderCenteredString(xPos, yPos, text, 0xFFFFFF);
+        return renderCenteredString(xPos, yPos, text, Color.white);
     }
     
     /**
@@ -30,7 +32,7 @@ public class TextRenderer {
      * @deprecated use {@link #renderString(int, int, String, int, TextAlignment)} with TextAlignment.CENTER instead
      */
     @Deprecated
-    public static int renderCenteredString(int xPos, int yPos, String text, int color) {
+    public static int renderCenteredString(int xPos, int yPos, String text, Color color) {
         return renderString(xPos - getFontRenderer().getStringWidth(text) / 2, yPos, text, color, TextAlignment.LEFT);
     }
 
@@ -39,21 +41,21 @@ public class TextRenderer {
      * See {@link #renderString(int, int, String, int, boolean)}
      */
     public static int renderString(int xPos, int yPos, String text) {
-        return renderString(xPos, yPos, text, 0xFFFFFF, TextAlignment.LEFT);
+        return renderString(xPos, yPos, text, Color.white, TextAlignment.LEFT);
     }
 
     public static int renderString(int xPos, int yPos, String text, TextAlignment align){
-        return renderString(xPos, yPos, text, 0xFFFFFF, align);
+        return renderString(xPos, yPos, text, Color.white, align);
     }
     
-    public static int renderString(int xPos, int yPos, String text, int color) {
+    public static int renderString(int xPos, int yPos, String text, Color color) {
         return renderString(xPos, yPos, text, color, false, TextAlignment.LEFT);
     }
     
     /**
      * See {@link #renderString(int, int, String, int, boolean)}
      */
-    public static int renderString(int xPos, int yPos, String text, int color, TextAlignment align) {
+    public static int renderString(int xPos, int yPos, String text, Color color, TextAlignment align) {
         return renderString(xPos, yPos, text, color, false, align);
     }
 
@@ -67,14 +69,14 @@ public class TextRenderer {
      * @param shadow
      * @return X position of rendered string
      */
-    public static int renderString(int xPos, int yPos, String text, int color, boolean shadow, TextAlignment align) {
+    public static int renderString(int xPos, int yPos, String text, Color color, boolean shadow, TextAlignment align) {
         switch(align){
             case LEFT:
-                return getFontRenderer().drawString(text, xPos, yPos, color, shadow);
+                return getFontRenderer().drawString(text, xPos, yPos, color.getRGB(), shadow);
             case CENTER:
-                return getFontRenderer().drawString(text, xPos - getFontRenderer().getStringWidth(text) / 2, yPos, color, shadow);
+                return getFontRenderer().drawString(text, xPos - getFontRenderer().getStringWidth(text) / 2, yPos, color.getRGB(), shadow);
             case RIGHT:
-                return getFontRenderer().drawString(text, xPos - getFontRenderer().getStringWidth(text), yPos, color, shadow);
+                return getFontRenderer().drawString(text, xPos - getFontRenderer().getStringWidth(text), yPos, color.getRGB(), shadow);
         }
         return -1;
     }
