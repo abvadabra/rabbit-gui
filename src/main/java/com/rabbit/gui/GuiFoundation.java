52d1b36ac50cdb0fd33036c75e07103de2701056
RedEnergy <forwot163@gmail.com>
2015-11-27 22:00:54 +0400

    GuiFoundation: Added methods to manipulate current Stage

--- src/main/java/com/rabbit/gui/GuiFoundation.java
+++ src/main/java/com/rabbit/gui/GuiFoundation.java
@@ -1,15 +1,39 @@
 package com.rabbit.gui;
 
+import com.rabbit.gui.base.Stage;
+import com.rabbit.gui.show.IShow;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import net.minecraft.client.Minecraft;
 
-@Mod(modid = "rabbit-gui", name = "Rabbit Gui Library by RedEnergy")
+@Mod(modid = "rabbit-gui", name = "Rabbit Gui Library")
 public class GuiFoundation {
 
     @Mod.EventHandler
     public void postLoad(FMLPostInitializationEvent event) {
         FMLLog.info("Rabbit Gui has been successfuly initialized");
+    }
+
+    /**
+     * If there are any currently opened Stage it will display given show in it <br>
+     * Otherwise will create new Stage
+     * @param show
+     */
+    public static void display(IShow show){
+        Stage current = getCurrentStage();
+        if(current != null){
+            current.setShow(show);
+        } else {
+            Minecraft.getMinecraft().displayGuiScreen(new Stage(show));
+        }
+    }
+
+    /**
+     * Returns currently opened Stage, may be null
+     */
+    public static Stage getCurrentStage(){
+       return Minecraft.getMinecraft().currentScreen instanceof Stage ? (Stage)Minecraft.getMinecraft().currentScreen : null;
     }
 
 }
