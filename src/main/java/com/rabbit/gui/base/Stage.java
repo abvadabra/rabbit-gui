1c4179f51e87437e425853ab2b64d3492062a043
RedEnergy <forwot163@gmail.com>
2015-11-28 15:35:29 +0400

    Stage: Added show history and methods to manipulate it. Few code cleanup

--- src/main/java/com/rabbit/gui/base/Stage.java
+++ src/main/java/com/rabbit/gui/base/Stage.java
@@ -7,29 +7,62 @@ import com.rabbit.gui.show.IShow;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 
+import java.util.*;
+
 public class Stage extends GuiScreen{
     
     protected IShow show;
-    protected boolean hasBeenInitialized;
+    protected boolean hasBeenInitialized = false;
+    private Stack<IShow> showHistory = new Stack<>();
     
     public Stage(){}
     
     public Stage(IShow show){
-        this.show = show;
-        this.show.setStage(this);
+        display(show);
     }
 
     public void reinitShow(){
         show.setSize(width, height);
         if(show instanceof WidgetContainer) ((WidgetContainer)show).getComponentsList().clear();
-        if (!hasBeenInitialized) {
+        if (!show.hasBeenInitialized()) {
             show.onInit();
-            hasBeenInitialized = true;
         }
         show.setup();
         if(show instanceof WidgetContainer) ((WidgetContainer)show).getComponentsList().forEach(component -> component.setup());
     }
-    
+
+    /**
+     * Puts the given show on stage
+     * @param show
+     */
+    public void display(IShow show){
+        setShow(show);
+        show.setStage(this);
+        pushHistory(show);
+        reinitShow();
+    }
+
+    public Stage setShow(IShow show){
+        this.show = show;
+        return this;
+    }
+
+    /**
+     * Updated stage's history and adds the given show <br>
+     * If given show already in the history it will be moved to the start
+     * @param show
+     */
+    private void pushHistory(IShow show){
+        if(this.showHistory.contains(show)){
+            this.showHistory.remove(show);
+        }
+        this.showHistory.push(show);
+    }
+
+    public Stack<IShow> getShowHistory(){
+        return this.showHistory;
+    }
+
     @Override
     public void drawScreen(int p_73863_1_, int p_73863_2_, float p_73863_3_) {
         show.onDraw(p_73863_1_, p_73863_2_, p_73863_3_);
@@ -80,11 +113,6 @@ public class Stage extends GuiScreen{
     @Override
     public boolean doesGuiPauseGame() {
         return false;
-    }
-    
-    public Stage setShow(IShow show){
-        this.show = show;
-        return this;
     }
     
     public IShow getShow(){
