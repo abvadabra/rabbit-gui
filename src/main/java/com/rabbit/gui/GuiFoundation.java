ae538f18454424f40eeb73b80012fe25b52492ac
RedEnergy <forwot163@gmail.com>
2015-12-19 23:39:10 +0400

    Version changed to v1.2.1

--- src/main/java/com/rabbit/gui/GuiFoundation.java
+++ src/main/java/com/rabbit/gui/GuiFoundation.java
@@ -7,7 +7,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import net.minecraft.client.Minecraft;
 
-@Mod(modid = "rabbit-gui", name = "Rabbit Gui Library", version = "v1.2.0")
+@Mod(modid = "rabbit-gui", name = "Rabbit Gui Library", version = "v1.2.1")
 public class GuiFoundation {
 
     @Mod.EventHandler
