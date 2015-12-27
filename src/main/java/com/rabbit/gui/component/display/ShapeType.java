b112319cb65f785d0934ac6628c31a388a77aabe
RedEnergy <forwot163@gmail.com>
2015-12-27 18:23:17 +0400

    Implemented enum with shape forms

---
+++ src/main/java/com/rabbit/gui/component/display/ShapeType.java
@@ -0,0 +1,29 @@
+package com.rabbit.gui.component.display;
+
+import com.rabbit.gui.render.Renderer;
+
+/**
+ * Contains shape forms which can be used in Shape.class component
+ */
+public enum ShapeType {
+    RECT((x, y, width, height, color) -> Renderer.drawRect(x, y, x + width, y + height, color)),
+    TRIANGLE((x, y, width, height, color) -> Renderer.drawTriangle(x, y + height, x + width / 2, y, x + width, y + height, color)),
+    CIRCLE((x, y, width, height, color) -> {
+        int side = Math.max(width, height);
+        Renderer.drawFilledArc(x + side / 2, y + side / 2, side / 2, 0, 360, color);
+    });
+
+    private ShapeDrawer drawer;
+
+    ShapeType(ShapeDrawer drawer){
+        this.drawer = drawer;
+    }
+
+    public void draw(int x, int y, int width, int height, int color) {
+        drawer.draw(x, y, width, height, color);
+    }
+
+    interface ShapeDrawer {
+        void draw(int x, int y, int width, int height, int color);
+    }
+}
